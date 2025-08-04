a6e2f61301d3c40297a9d793da27eed1dbb80487
Mohammad <makbariengineer@gmail.com>
2025-08-04 12:28:14 +0330

    # Added extentions on Code

--- SwiftBeginner/SwiftBeginner/main.swift
+++ SwiftBeginner/SwiftBeginner/main.swift
@@ -1,10 +1,21 @@
 //
 //  main.swift
-//  SwiftBeginner
+//  Swift
 //
 //  Created by Mohammad on 7/26/25.
 //
 
 import Foundation
 
-dictionaryMethod()
+let fibs = [0, 1, 1, 2, 3, 5]
+
+var result = 0
+
+let map = fibs.map { element in
+    result = result + element
+    return result
+}
+
+print(map)
+
+// Like AND Sub TNX
