aef68851a02b46534be9de15580627cc572f705f
Mohammad <makbariengineer@gmail.com>
2025-08-08 17:07:50 +0330

    #Added new code try change array

--- SwiftBeginner/SwiftBeginner/Algorithem.swift
+++ SwiftBeginner/SwiftBeginner/Algorithem.swift
@@ -6,3 +6,88 @@
 //
 
 import Foundation
+
+//func totalFruit(_ fruits: [Int]) -> Int {
+//    var maxCount: Int = 0
+//    var basket: [Int: Int] = [:]
+//    var left = 0
+//    
+//    for (right, friut) in fruits.enumerated() {
+//        basket[friut] = (basket[friut] ?? 0) + 1
+//        
+//        while basket.count > 2 {
+//            let leftfriut = fruits[left]
+//            basket[leftfriut]! -= 1
+//            if basket[leftfriut] == 0 {
+//                basket.removeValue(forKey: leftfriut)
+//            }
+//            
+//            left += 1
+//        }
+//        maxCount = max(maxCount, right - left + 1)
+//    }
+//    return maxCount
+//}
+//
+////let data = totalFruit([1,2,3,2,2])
+////print(data)
+//
+//func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+// []
+//}
+//
+//let sum = twoSum([3,2,4], 6)
+//print(sum)
+//
+//
+//func mathNumber(_ numbers: [Int]) -> Int {
+//    let filter = numbers.filter({ $0 % 2 == 0})
+//    return filter.reduce(0, +)
+//}
+//
+//let num = mathNumber([1, 2, 3, 4])
+//
+//print(num)
+//
+//var myArray = [-1, 0, 5, -2, 3]
+//var result: Int = 0
+//myArray.forEach { element in
+//    if element > 0 {
+//        result = result + 1
+//    }
+//}
+//
+//print(result)
+//
+//let myLet = "swwift"
+//
+//let frq = myLet.frequencies
+//
+//frq.forEach { key, value in
+//    if value > 1 {
+//        print(key)
+//    } else {
+//        print("key: \(key) = value: \(value)")
+//    }
+//}
+//
+//let one = "listen".sorted(by: <)
+//let two = "silent".sorted(by: <)
+//let final = one == two
+//print(final)
+//
+//
+//let reversArray = [1, 2, 3, 4]//.reversed()
+//var reResult: [Int] = []
+//
+//_ = reversArray.map({ element in
+//    if element < reversArray.last ?? 0 {
+//        reResult.append(element)
+//    } else {
+//        reResult.append(reversArray.last ?? 0)
+//    }
+//})
+//
+//print(reResult)
+//
+//let lettersss = "abcabcbb"
--- SwiftBeginner/SwiftBeginner/main.swift
+++ SwiftBeginner/SwiftBeginner/main.swift
@@ -7,15 +7,3 @@
 
 import Foundation
 
-let fibs = [0, 1, 1, 2, 3, 5]
-
-var result = 0
-
-let map = fibs.map { element in
-    result = result + element
-    return result
-}
-
-print(map)
-
-// Like AND Sub TNX
